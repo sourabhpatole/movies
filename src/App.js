e2ab38a0d8f36d76c556b3c86dfb2e69ed9068ea
sourabh <51184750+sourabhpatole@users.noreply.github.com>
2022-01-23 10:38:58 +0530

    master

--- src/App.js
+++ src/App.js
@@ -89,58 +89,58 @@ export default function App() {
     <div>
       <div className="App">
         <input
-          className="form-label my-3"
+          className="form-label mx-3 my-4"
           value={name}
           onChange={(event) => setName(event.target.value)}
           placeholder="Enter a name"
         />
         <input
-          className="form-label my-3"
+          className="form-label mx-3 my-4"
           value={poster}
           onChange={(event) => setPoster(event.target.value)}
           placeholder="Enter a poster"
         />
         <input
-          className="form-label my-3"
+          className="form-label mx-3 my-4"
           value={rating}
           onChange={(event) => setRating(event.target.value)}
           placeholder="Enter a rating"
         />
         <input
-          className="form-label my-3"
+          className="form-label mx-3 my-4"
           value={summary}
           onChange={(event) => setSummary(event.target.value)}
           placeholder="Enter a summery"
         />
         <input
-          className="form-label my-3"
+          className="form-label mx-3 my-4"
           value={trailer}
           onChange={(event) => setTrailer(event.target.value)}
           placeholder="Enter a trailer"
         />
+        <button
+          className="btn btn-primary my-3"
+          onClick={() => {
+            // console.log({
+            //   name: name,
+            //   poster: poster,
+            //   rating: rating,
+            //   summary: summary,
+            //   trailer: trailer,
+            // });
+            const newMovie = {
+              name: name,
+              poster: poster,
+              rating: rating,
+              summary: summary,
+              trailer: trailer,
+            };
+            setMovieList([...movieList, newMovie]);
+          }}
+        >
+          Add Movie
+        </button>
       </div>
-      <button
-        className="btn btn-primary"
-        onClick={() => {
-          // console.log({
-          //   name: name,
-          //   poster: poster,
-          //   rating: rating,
-          //   summary: summary,
-          //   trailer: trailer,
-          // });
-          const newMovie = {
-            name: name,
-            poster: poster,
-            rating: rating,
-            summary: summary,
-            trailer: trailer,
-          };
-          setMovieList([...movieList, newMovie]);
-        }}
-      >
-        Add Movie
-      </button>
       <MovieList movies={movieList} setMovieList={setMovieList} />
       <AddColor />
       {/* <Counter /> */}
@@ -155,7 +155,7 @@ function AddColor() {
   return (
     <div className="mb-3 my-3">
       <input
-        className="form-label my-3"
+        className="form-label mx-3 my-4"
         value={color}
         onChange={(event) => setColor(event.target.value)}
         style={styles}
@@ -180,5 +180,5 @@ function ColorBox({ clr }) {
     background: clr,
     margin: "10px",
   };
-  return <div className="form-label my-3 mb-3 " style={styles}></div>;
+  return <div className="form-label mx-3 my-4 mb-3 " style={styles}></div>;
 }
