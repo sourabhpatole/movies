2e0e86ce80045cc4c6a16aa8ff33c23a92028907
sourabh <51184750+sourabhpatole@users.noreply.github.com>
2022-02-18 18:46:32 +0530

    api change global

--- src/global.js
+++ src/global.js
@@ -1,5 +1,5 @@
-// export const API = "https://projectmoviepro.herokuapp.com";
-export const API = "http://localhost:3000";
+export const API = "https://projectmoviepro.herokuapp.com";
+// export const API = "http://localhost:3000";
 
 //3000 is node js port
 //3001 is react js port
